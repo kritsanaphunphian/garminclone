9757d8b923c9e1d147ca9fbbe928f7a671150a5b
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-02-19 00:12:00 +0700

    Flatsome-Child theme, code styling & remove unnecessary if condition block.

--- src/themes/flatsome-child/wp-addtional-info-catalog-page.php
+++ src/themes/flatsome-child/wp-addtional-info-catalog-page.php
@@ -1,18 +1,18 @@
 <?php
 
 function custom_shop_display_post_meta( $atts ) {
-    
-        global $product;
-        
-        // replace the custom field name with your own
-        $catalog_description = get_post_meta( $product->id, 'catalog_description', true );
-        
-        // Add these fields to the shop loop if set
-        if ( ! empty( $catalog_description ) ) {
-            echo '<div class="box-excerpt is-small catalog-short">' . $catalog_description . '</div>';
-            echo '<p>';
-        }if( empty( $catalog_description ) ){
-            echo '<p><p>';
-        }
+    global $product;
+
+    // replace the custom field name with your own
+    $catalog_description = get_post_meta( $product->id, 'catalog_description', true );
+
+    // Add these fields to the shop loop if set
+    if ( ! empty( $catalog_description ) ) {
+        echo '<div class="box-excerpt is-small catalog-short">' . $catalog_description . '</div>';
+        echo '<p>';
+    } else {
+        echo '<p><p>';
     }
-add_action('flatsome_product_box_after', 'custom_shop_display_post_meta', 20);
+}
+
+add_action( 'flatsome_product_box_after', 'custom_shop_display_post_meta', 20 );
