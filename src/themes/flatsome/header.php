8ce9ed89edca5fb624e887051458e96c00890c13
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-03-05 17:12:16 +0700

    Remove custom-code from Flatsome's header, move it to a proper place.

--- src/themes/flatsome/header.php
+++ src/themes/flatsome/header.php
@@ -8,30 +8,16 @@
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php wp_head(); ?>
 
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); // Body classes is added from inc/helpers-frontend.php ?>>
+
 <a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'flatsome' ); ?></a>
 
 <div id="wrapper">
-<div class="ajax-loader">
-<div class="sk-fading-circle">
-<div class="sk-circle1 sk-circle"></div>
-<div class="sk-circle2 sk-circle"></div>
-<div class="sk-circle3 sk-circle"></div>
-<div class="sk-circle4 sk-circle"></div>
-<div class="sk-circle5 sk-circle"></div>
-<div class="sk-circle6 sk-circle"></div>
-<div class="sk-circle7 sk-circle"></div>
-<div class="sk-circle8 sk-circle"></div>
-<div class="sk-circle9 sk-circle"></div>
-<div class="sk-circle10 sk-circle"></div>
-<div class="sk-circle11 sk-circle"></div>
-<div class="sk-circle12 sk-circle"></div>
-</div>
-</div>
+
 <?php do_action('flatsome_before_header'); ?>
 
 <header id="header" class="header <?php flatsome_header_classes();  ?>">
