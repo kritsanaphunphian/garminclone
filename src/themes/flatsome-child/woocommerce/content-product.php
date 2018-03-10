8c0b01c0187d28ad7f983df424ba0463972a6e0a
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-03-10 16:57:45 +0700

    Duplicate 'content-product.php' template from Flatsom to Flatsome-child (with proper code styling).

---
+++ src/themes/flatsome-child/woocommerce/content-product.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The template for displaying product content within loops.
+ *
+ * @see wp-content/themes/flatsome/woocommerce/content-product.php
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+global $product;
+
+// Ensure visibility
+if ( empty( $product ) || ! $product->is_visible() ) {
+    return;
+}
+
+// Check stock status
+$out_of_stock = 'outofstock' == get_post_meta( $post->ID, '_stock_status', true );
+
+// Extra post classes
+$classes   = array();
+$classes[] = 'product-small';
+$classes[] = 'col';
+$classes[] = 'has-hover';
+
+if ( $out_of_stock ) $classes[] = 'out-of-stock';
+
+?>
+
+<div <?php post_class( $classes ); ?>>
+    <div class="col-inner">
+
+        <?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
+
+        <div class="product-small box <?php echo flatsome_product_box_class(); ?>">
+            <div class="box-image">
+                <div class="<?php echo flatsome_product_box_image_class(); ?>">
+                    <a href="<?php echo get_the_permalink(); ?>">
+                        <?php
+                        /**
+                         * @hooked woocommerce_get_alt_product_thumbnail - 11
+                         * @hooked woocommerce_template_loop_product_thumbnail - 10
+                         */
+                        do_action( 'flatsome_woocommerce_shop_loop_images' );
+                        ?>
+                    </a>
+                </div>
+                <div class="image-tools is-small top right show-on-hover">
+                    <?php do_action( 'flatsome_product_box_tools_top' ); ?>
+                </div>
+                <div class="image-tools is-small hide-for-small bottom left show-on-hover">
+                    <?php do_action( 'flatsome_product_box_tools_bottom' ); ?>
+                </div>
+                <div class="image-tools <?php echo flatsome_product_box_actions_class(); ?>">
+                    <?php  do_action( 'flatsome_product_box_actions' ); ?>
+                </div>
+                <?php if ( $out_of_stock ) : ?>
+                    <div class="out-of-stock-label"><?php _e( 'Out of stock', 'woocommerce' ); ?></div>
+                <?php endif; ?>
+            </div><!-- box-image -->
+
+            <div class="box-text <?php echo flatsome_product_box_text_class(); ?>">
+                <?php do_action( 'woocommerce_before_shop_loop_item_title' ); ?>
+
+                <div class="title-wrapper">
+                    <?php do_action( 'woocommerce_shop_loop_item_title' ); ?>
+                </div>
+
+                <div class="price-wrapper">
+                    <?php do_action( 'woocommerce_after_shop_loop_item_title' ); ?>
+                </div>
+
+                <?php do_action( 'flatsome_product_box_after' ); ?>
+            </div><!-- box-text -->
+        </div><!-- box -->
+
+        <?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
+
+    </div><!-- .col-inner -->
+</div><!-- col -->
