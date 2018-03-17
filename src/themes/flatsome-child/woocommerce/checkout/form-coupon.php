a949eabebc8a5796bd780ab103bff9ab8fa07c2f
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-03-17 22:49:42 +0700

    flatsome-child/woocommerce/checkout/form-coupon.php, convert tab-indent to space.

--- src/themes/flatsome-child/woocommerce/checkout/form-coupon.php
+++ src/themes/flatsome-child/woocommerce/checkout/form-coupon.php
@@ -2,17 +2,17 @@
 /**
  * Checkout coupon form
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
  * @version     2.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+    exit; // Exit if accessed directly
 }
 
 if ( ! WC()->cart->coupons_enabled() ) {
-	return;
+    return;
 }
 
 $info_message = apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Click here to enter your code', 'woocommerce' ) . '</a>' );
@@ -20,14 +20,14 @@ wc_print_notice( '<span class="widget-title"><i class="icon-tag"></i></span>'.$i
 ?>
 
 <form class="checkout_coupon has-border is-dashed" method="post" style="display:none">
-	<div class="coupon">
-		<div class="flex-row medium-flex-wrap">
-			<div class="flex-col flex-grow">
-				<input type="text" name="coupon_code" class="input-text" placeholder="<?php _e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
-			</div>
-			<div class="flex-col">
-				<input type="submit" class="button expand" name="apply_coupon" value="<?php _e( 'Apply coupon', 'woocommerce' ); ?>" />
-			</div>
-		</div><!-- row -->
-	</div><!-- coupon -->
+    <div class="coupon">
+        <div class="flex-row medium-flex-wrap">
+            <div class="flex-col flex-grow">
+                <input type="text" name="coupon_code" class="input-text" placeholder="<?php _e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
+            </div>
+            <div class="flex-col">
+                <input type="submit" class="button expand" name="apply_coupon" value="<?php _e( 'Apply coupon', 'woocommerce' ); ?>" />
+            </div>
+        </div><!-- row -->
+    </div><!-- coupon -->
 </form>
