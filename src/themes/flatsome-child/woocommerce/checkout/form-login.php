71663bcc79ec98a9271a43ffa1979a281b839b11
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-03-17 23:24:32 +0700

    Checkout page, make the 'returning-customer-login-here' message align center.

--- src/themes/flatsome-child/woocommerce/checkout/form-login.php
+++ src/themes/flatsome-child/woocommerce/checkout/form-login.php
@@ -10,8 +10,10 @@ if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_lo
     return;
 }
 
-$info_message  = apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'woocommerce' ) );
+$info_message  = '<div class="garminbygis-wc-checkout-must-be-logged-in-message">';
+$info_message .= apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'woocommerce' ) );
 $info_message .= ' <a href="#" class="showlogin">' . __( 'Click here to login', 'woocommerce' ) . '</a>';
+$info_message .= '</div>';
 wc_print_notice( $info_message, 'notice' );
 
 woocommerce_login_form(
