b83f0f2e0b754fd93dbdc89bf56db1b972c687ec
Nuttanon Trapnirundon <iam.nuttanon@gmail.com>
2018-04-02 11:51:11 +0700

    single-product/product-attributes.php, set label column's width to 60%.

--- src/themes/flatsome-child/woocommerce/single-product/product-attributes.php
+++ src/themes/flatsome-child/woocommerce/single-product/product-attributes.php
@@ -30,14 +30,14 @@ ob_start();
 
 	<?php if ( $display_dimensions && $product->has_weight() && !$dimension_group ) : $dimension_output = true; ?>
 		<tr>
-			<th><?php _e( 'Weight', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Weight', 'woocommerce' ) ?></th>
 			<td class="product_weight"><?php echo esc_html( wc_format_weight( $product->get_weight() ) ); ?></td>
 		</tr>
 	<?php endif; ?>
 
 	<?php if ( $display_dimensions && $product->has_dimensions() && !$dimension_group ) : $dimension_output = true; ?>
 		<tr>
-			<th><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
 			<td class="product_dimensions"><?php echo esc_html( wc_format_dimensions( $product->get_dimensions( false ) ) ); ?></td>
 		</tr>
 	<?php endif; ?>
@@ -57,7 +57,7 @@ ob_start();
 		}
 		?>
 		<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
-			<th><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
+			<th width="60%"><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
 			<td><?php
 				if ( $attribute['is_taxonomy'] ) {
 
@@ -93,14 +93,14 @@ foreach($grouped_attributes as $grouped):
 
 	<?php if ( $display_dimensions && $product->has_weight() && $dimension_group == $grouped['name'] ) : $dimension_output = true; ?>
 		<tr>
-			<th><?php _e( 'Weight', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Weight', 'woocommerce' ) ?></th>
 			<td class="product_weight"><?php echo esc_html( wc_format_weight( $product->get_weight() ) ); ?></td>
 		</tr>
 	<?php endif; ?>
 
 	<?php if ( $display_dimensions && $product->has_dimensions() && $dimension_group == $grouped['name'] ) : $dimension_output = true; ?>
 		<tr>
-			<th><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
 			<td class="product_dimensions"><?php echo esc_html( wc_format_dimensions( $product->get_dimensions( false ) ) ); ?></td>
 		</tr>
 	<?php endif; ?>
@@ -108,7 +108,7 @@ foreach($grouped_attributes as $grouped):
 	<?php foreach($grouped->get_grouped_terms() as $attribute): ?>
 
 		<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
-			<th><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
+			<th width="60%"><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
 			<td><?php
 
 				$values = array();
@@ -153,14 +153,14 @@ foreach($grouped_attributes as $grouped):
 
 	<?php if ( $display_dimensions && $product->has_weight()) : ?>
 		<tr>
-			<th><?php _e( 'Weight', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Weight', 'woocommerce' ) ?></th>
 			<td class="product_weight"><?php echo esc_html( wc_format_weight( $product->get_weight() ) ); ?></td>
 		</tr>
 	<?php endif; ?>
 
 	<?php if ( $display_dimensions && $product->has_dimensions()) : ?>
 		<tr>
-			<th><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
+			<th width="60%"><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
 			<td class="product_dimensions"><?php echo esc_html( wc_format_dimensions( $product->get_dimensions( false ) ) ); ?></td>
 		</tr>
 	<?php endif; ?>
